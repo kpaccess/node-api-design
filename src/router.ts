bbe20b443cc0000047b7edc35e3f34e8f901f726
krishna pradhan <kpaccess@gmail.com>
2024-05-23 10:01:21 -0400

    initial setup

---
+++ src/router.ts
@@ -0,0 +1,52 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+  console.log("Hello World!");
+  res.json({ message: "Hello World!" });
+});
+/**
+ * Product
+ */
+router.get("/product", (req: Request, res: Response) => {
+  res.json({ message: "product" });
+});
+
+router.get("/product/:id", (req: Request, res: Response) => {});
+
+router.post("/product", (req: Request, res: Response) => {});
+
+router.put("/product/:id", (req: Request, res: Response) => {});
+
+router.delete("/product/:id", (req: Request, res: Response) => {});
+
+/**
+ * Update
+ */
+
+router.get("/update", (req: Request, res: Response) => {});
+
+router.get("/update/:id", (req: Request, res: Response) => {});
+
+router.post("/update", (req: Request, res: Response) => {});
+
+router.put("/update/:id", (req: Request, res: Response) => {});
+
+router.delete("/update/:id", (req: Request, res: Response) => {});
+
+/**
+ * UpdatePoint
+ */
+
+router.get("/updatepoint", (req: Request, res: Response) => {});
+
+router.get("/updatepoint/:id", (req: Request, res: Response) => {});
+
+router.post("/updatepoint", (req: Request, res: Response) => {});
+
+router.put("/updatepoint/:id", (req: Request, res: Response) => {});
+
+router.delete("/updatepoint/:id", (req: Request, res: Response) => {});
+
+export default router;
